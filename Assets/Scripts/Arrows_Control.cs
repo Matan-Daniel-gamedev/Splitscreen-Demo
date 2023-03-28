0a8d232ea78222c31b157d94e5adde576a996801
danigil <dgilkarov33@gmail.com>
2023-03-28 23:29:16 +0300

    trying to fix linting

--- Assets/Scripts/Arrows_Control.cs
+++ Assets/Scripts/Arrows_Control.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Arrows_Control : MonoBehaviour
 {
     Vector3 Vec;
+    float speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +15,19 @@ public class Arrows_Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vec = transform.localPosition;  
+        Vec = transform.localPosition;
          if (Input.GetKey(KeyCode.LeftArrow))
-            Vec.x -= Time.deltaTime * 20;  
+            Vec.x -= Time.deltaTime * speed;
+
          if (Input.GetKey(KeyCode.RightArrow))
-            Vec.x += Time.deltaTime * 20;
+            Vec.x += Time.deltaTime * speed;
+
          if (Input.GetKey(KeyCode.UpArrow))
-            Vec.y += Time.deltaTime * 20;  
+            Vec.y += Time.deltaTime * speed;
+
          if (Input.GetKey(KeyCode.DownArrow))
-            Vec.y -= Time.deltaTime * 20;  
+            Vec.y -= Time.deltaTime * speed;
+            
         transform.localPosition = Vec;
     }
 }
--- Assets/Scripts/WASD_Control.cs
+++ Assets/Scripts/WASD_Control.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WASD_Control : MonoBehaviour
 {
     Vector3 Vec;
+    float speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +17,17 @@ public class WASD_Control : MonoBehaviour
     {
         Vec = transform.localPosition;  
          if (Input.GetKey(KeyCode.A))
-            Vec.x -= Time.deltaTime * 20;  
+            Vec.x -= Time.deltaTime * speed;  
+
          if (Input.GetKey(KeyCode.D))
-            Vec.x += Time.deltaTime * 20;
+            Vec.x += Time.deltaTime * speed;
+
          if (Input.GetKey(KeyCode.W))
-            Vec.y += Time.deltaTime * 20;  
+            Vec.y += Time.deltaTime * speed;
+
          if (Input.GetKey(KeyCode.S))
-            Vec.y -= Time.deltaTime * 20;  
+            Vec.y -= Time.deltaTime * speed;
+            
         transform.localPosition = Vec;
     }
 }
